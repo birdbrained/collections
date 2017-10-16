d6effcb53054c73318f0ea788d4eb0119b6599c4
Josh Bird <joshua.thomas.bird@gmail.com>
2017-10-16 11:13:24 -0400

    Clean out unnecessary code

--- app/components/item-display/component.js
+++ app/components/item-display/component.js
@@ -3,12 +3,5 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-    editing: true,
-
-    description: 'Enter a title for the preprint.',
-
-    didReceiveAttrs() {
-        this.set('description', this.attrs.description);
-    },
 
 });
